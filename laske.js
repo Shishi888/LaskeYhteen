e311623eb5e71d800311a1c9da02dd6ebb4b16ba
Shishi888 <122361934+Shishi888@users.noreply.github.com>
2023-02-06 15:56:52 +0200

    Add files via upload

---
+++ laske.js
@@ -0,0 +1,5 @@
+function LaskeYhteen() {  
+     var numero1 = Number(document.getElementById("ekanum").value);     
+     var numero2 = Number(document.getElementById("tokanum").value);     
+     var summa = numero1 + numero2;  
+      document.getElementById("tulos").innerHTML = summa; }
